b4d52af04e36948d4e1e1227c65a2fe2d21d243a
Tomaz Treven <treven@nefta.io>
2024-05-13 16:09:00 +0200

    Support for video-interstitials Other smaller improvements The version is baked in aar itself instead of the file name Logging on iOS will log in console and not only to XCode debug

--- Assets/Nefta/Editor/NeftaConfigurationInspector.cs
+++ Assets/Nefta/Editor/NeftaConfigurationInspector.cs
@@ -1,7 +1,10 @@
 using System;
 using System.IO;
+using System.IO.Compression;
+using System.Xml;
 using UnityEditor;
 using UnityEngine;
+using UnityEditor.iOS.Xcode;
 
 namespace Nefta.Editor
 {
@@ -10,6 +13,12 @@ namespace Nefta.Editor
     {
         private NeftaConfiguration _configuration;
         private bool _isLoggingEnabled;
+        
+        private string _error;
+        private string _androidAdapterVersion;
+        private string _androidVersion;
+        private string _iosAdapterVersion;
+        private string _iosVersion;
         
         private static PluginImporter GetImporter(bool debug)
         {
@@ -30,6 +39,12 @@ namespace Nefta.Editor
                 EditorUtility.SetDirty(_configuration);
                 AssetDatabase.SaveAssetIfDirty(_configuration);
             }
+            
+            _error = null;
+#if UNITY_2021
+            GetAndroidVersions();
+#endif
+            GetIosVersions();
         }
 
         public void OnDisable()
@@ -39,6 +54,29 @@ namespace Nefta.Editor
         
         public override void OnInspectorGUI()
         {
+            if (_error != null)
+            {
+                EditorGUILayout.LabelField(_error, EditorStyles.helpBox);
+                return;
+            }
+            
+#if UNITY_2021
+            if (_androidAdapterVersion != _iosAdapterVersion)
+            {
+                DrawVersion("Nefta AdMob Android Custom Adapter version", _androidAdapterVersion);
+                DrawVersion("Nefta SDK Android version", _androidVersion);
+                EditorGUILayout.Space(5);
+                DrawVersion("Nefta AdMob iOS Custom Adapter version", _iosAdapterVersion);
+                DrawVersion("Nefta SDK iOS version", _iosVersion);
+            }
+            else
+#endif
+            {
+                DrawVersion("Nefta AdMob Custom Adapter version", _androidAdapterVersion);
+                DrawVersion("Nefta SDK version", _androidVersion);
+            }
+            EditorGUILayout.Space(5);
+            
             base.OnInspectorGUI();
             if (_isLoggingEnabled != _configuration._isLoggingEnabled)
             {
@@ -113,5 +151,105 @@ namespace Nefta.Editor
             }
         }
         
+        private static void DrawVersion(string label, string version)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(label); 
+            EditorGUILayout.LabelField(version, EditorStyles.boldLabel, GUILayout.Width(60)); 
+            EditorGUILayout.EndHorizontal();
+        }
+    
+#if UNITY_2021
+        private void GetAndroidVersions()
+        {
+            var guids = AssetDatabase.FindAssets("NeftaAMAdapter");
+            if (guids.Length == 0)
+            {
+                _error = "NeftaAMAdapter AARs not found in project";
+                return;
+            }
+            if (guids.Length > 2)
+            {
+                _error = "Multiple instances of NeftaAMAdapter AARs found in project";
+                return;
+            }
+            var aarPath = AssetDatabase.GUIDToAssetPath(guids[0]);
+            using ZipArchive aar = ZipFile.OpenRead(aarPath);
+            ZipArchiveEntry manifestEntry = aar.GetEntry("AndroidManifest.xml");
+            if (manifestEntry == null)
+            {
+                _error = "Nefta SDK AAR seems to be corrupted";
+                return;
+            }
+            using Stream manifestStream = manifestEntry.Open();
+            XmlDocument manifest = new XmlDocument();
+            manifest.Load(manifestStream);
+            var root = manifest.DocumentElement;
+            if (root == null)
+            {
+                _error = "Nefta SDK AAR seems to be corrupted";
+                return;
+            }
+            _androidAdapterVersion = root.Attributes["android:versionName"].Value;
+            var metaNodes = root.SelectNodes("/manifest/application/meta-data");
+            foreach (XmlNode metaNode in metaNodes)
+            {
+                var name = metaNode.Attributes["android:name"];
+                if (name.Value == "NeftaSDKVersion")
+                {
+                    _androidVersion = metaNode.Attributes["android:value"].Value;
+                    break;
+                }
+            }
+        }
+#endif
+        
+        private void GetIosVersions()
+        {
+            var guids = AssetDatabase.FindAssets("NeftaAdapter");
+            if (guids.Length == 0)
+            {
+                _error = "NeftaAdapter not found in project";
+                return;
+            }
+            if (guids.Length > 2)
+            {
+                _error = "Multiple instances of NeftaAdapter found in project";
+                return;
+            }
+            var wrapperPath = AssetDatabase.GUIDToAssetPath(guids[0]);
+            if (wrapperPath.EndsWith(".h"))
+            {
+                wrapperPath = AssetDatabase.GUIDToAssetPath(guids[1]);
+            }
+            using StreamReader reader = new StreamReader(wrapperPath);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains("GADVersionNumber version") && line.Contains(";"))
+                {
+                    var start = line.IndexOf('{') + 1;
+                    var end = line.LastIndexOf('}');
+                    _iosAdapterVersion = line.Substring(start, end - start).Replace(" ", "").Replace(',', '.');
+                    break;
+                }
+            }
+            
+            guids = AssetDatabase.FindAssets("NeftaSDK.xcframework");
+            if (guids.Length == 0)
+            {
+                _error = "NeftaSDK.xcframework not found in project";
+                return;
+            }
+            if (guids.Length > 1)
+            {
+                _error = "Multiple instances of NeftaSDK.xcframework found in project";
+                return;
+            }
+            var frameworkPath = AssetDatabase.GUIDToAssetPath(guids[0]);
+            var plist = new PlistDocument();
+            plist.ReadFromFile(frameworkPath + "/Info.plist");
+            _iosVersion = plist.root["Version"].AsString();
+        }
     }
 }
--- Assets/Nefta/Events/ResourceEvent.cs
+++ Assets/Nefta/Events/ResourceEvent.cs
@@ -8,15 +8,15 @@ namespace Nefta.Core.Events
         SoftCurrency,
         PremiumCurrency,
         Resource,
-        CoreItem,
-        CosmeticItem,
         Consumable,
-        Experience,
+        CosmeticItem,
+        CoreItem,
         Chest,
+        Experience,
         Other
     }
 
-    public class ResourceEvent : GameEvent
+    public abstract class ResourceEvent : GameEvent
     {
         private static readonly Dictionary<ResourceCategory, string> CategoryToString = new Dictionary<ResourceCategory, string>()
         {
@@ -24,48 +24,19 @@ namespace Nefta.Core.Events
             { ResourceCategory.SoftCurrency, "soft_currency" },
             { ResourceCategory.PremiumCurrency, "premium_currency" },
             { ResourceCategory.Resource, "resource" },
-            { ResourceCategory.CoreItem, "core_item" },
-            { ResourceCategory.CosmeticItem, "cosmetic_item" },
             { ResourceCategory.Consumable, "consumable" },
-            { ResourceCategory.Experience, "experience" },
+            { ResourceCategory.CosmeticItem, "cosmetic_item" },
+            { ResourceCategory.CoreItem, "core_item" },
             { ResourceCategory.Chest, "chest" },
-            { ResourceCategory.Other, "other" }
+            { ResourceCategory.Experience, "experience" },
+            { ResourceCategory.Other, "other" },
         };
-
-        /// <summary>
-        /// The name of the resource
-        /// </summary>
-        public string _name;
             
         /// <summary>
         /// The category of the resource
         /// </summary>
         public ResourceCategory _resourceCategory;
         
-        /// <summary>
-        /// Quantity that player received
-        /// </summary>
-        public int _quantity;
-            
-        /// <summary>
-        /// Any other custom data you want to track
-        /// </summary>
-        public string _customString;
-
-        protected ResourceEvent()
-        {
-                
-        }
-
-        public override RecordedEvent GetRecordedEvent()
-        {
-            return new RecordedEvent()
-            {
-                _category = CategoryToString[_resourceCategory],
-                _itemName = _name,
-                _value = _quantity,
-                _customPayload = _customString,
-            };
-        }
+        internal override string _category => CategoryToString[_resourceCategory];
     }
 }
